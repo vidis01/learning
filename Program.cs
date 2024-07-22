e8ad95239991e882b518897a2519b579fe169398
Vidmantas <jaguaras999@gmail.com>
2024-07-22 22:03:42 +0300

    code removed

--- Program.cs
+++ Program.cs
@@ -7,15 +7,7 @@ namespace Learning
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-
-            var namas = new Namas(13, "Žemaitės g.", "Vilnius");
-
-            namas.IdetiLaukinesDuris(new LaukinesDurys(true, 3, 2.5f, 1.5f, "Algio", SpalvaEnum.MĖLYNA));
-
-            foreach (var laukinesDurys in namas.LaukinesDurys)
-            {
-                Console.WriteLine(laukinesDurys.GetInfo());
-            }
+            
 
         }
     }
