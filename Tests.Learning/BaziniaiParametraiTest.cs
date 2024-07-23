48306e75097b59b6762c5178ce7b8dd413c1b1f1
Vidmantas <jaguaras999@gmail.com>
2024-07-23 20:03:03 +0300

    test sample

---
+++ Tests.Learning/BaziniaiParametraiTest.cs
@@ -0,0 +1,22 @@
+﻿using Learning.Classes;
+using Learning.Enums;
+
+namespace Tests.Learning
+{
+    public class BaziniaiParametraiTest
+    {
+
+        [Theory]
+        [InlineData( "jono durys",  SpalvaEnum.MĖLYNA,  "pavadinimas: jono durys, spalva: MĖLYNA")]
+        [InlineData( "petro durys",  SpalvaEnum.JUODA,  "pavadinimas: petro durys, spalva: JUODA")]
+        [InlineData( "algio durys",  SpalvaEnum.BALTA,  "pavadinimas: algio durys, spalva: BALTA")]
+        public void TestGetInfo(string pavadinimas, SpalvaEnum spalva, string expectedResult)
+        {
+            var baziniaiParametrai = new BaziniaiParametrai(pavadinimas, spalva);
+
+            var actualResult = baziniaiParametrai.GetInfo();
+
+            //Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
