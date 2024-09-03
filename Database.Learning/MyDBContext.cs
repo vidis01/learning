80c6218730a25e6ddc5f7e2b78ccdc9fc97d2643
Vidmantas <jaguaras999@gmail.com>
2024-09-03 16:47:42 +0300

    db stuff

--- Database.Learning/MyDBContext.cs
+++ Database.Learning/MyDBContext.cs
@@ -1,7 +1,13 @@
-﻿namespace Database.Learning
-{
-    public class MyDBContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace Database.Learning
+{
+    public class MyDBContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=DESKTOP-0B6P97H\SQLEXPRESS;Database=MyTestDB;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+        }
     }
 }
