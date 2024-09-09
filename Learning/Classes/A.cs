41f1fe3ce575b92f114fc4fe87db4138b42dfcd7
Mantas Pociunas <mpoc@inbox.lt>
2024-09-09 14:11:05 +0300

    added private string in class A

--- Learning/Classes/A.cs
+++ Learning/Classes/A.cs
@@ -7,6 +7,7 @@
         private string Name { get; set; }
         private string LastName { get; set; }
 
+        private string mantoAmzius = "no idea";
         private B b;
 
         public A()
