78b835cc3f90de7dd993f94eb74b8859d342b462
Vidmantas <jaguaras999@gmail.com>
2024-09-10 10:13:41 +0300

    add initial data to table

--- Database.Learning/MyDBContext.cs
+++ Database.Learning/MyDBContext.cs
@@ -1,5 +1,6 @@
 ﻿using Database.Learning.DbModels;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
 
 namespace Database.Learning
 {
@@ -17,6 +18,13 @@ namespace Database.Learning
         {
             optionsBuilder.UseSqlServer(
                 @"Server=DESKTOP-0B6P97H\SQLEXPRESS;Database=MyTestDB;Trusted_Connection=True;TrustServerCertificate=True;ConnectRetryCount=0");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Shipper>().HasData(new Shipper { ShipperName = "Speedy Express", Phone = "(503) 555-9831" });
+            modelBuilder.Entity<Shipper>().HasData(new Shipper { ShipperName = "United Package", Phone = "(503) 555-3199" });
+            modelBuilder.Entity<Shipper>().HasData(new Shipper { ShipperName = "Federal Shipping", Phone = "(503) 555-9931" });
         }
     }
 }
