856c217af0e2f830d51222730b9fc85b86ed5e16
Vidmantas <jaguaras999@gmail.com>
2024-10-02 18:57:21 +0300

    inheritence sample

--- Learning/Classes/Selector.cs
+++ Learning/Classes/Selector.cs
@@ -101,4 +101,48 @@
             return (TopicEnum)rand.Next(1, 4);
         }
     }
+
+    public class WordSelector : IWordSelector
+    {
+        private Random _random;
+        protected Dictionary<TopicEnum, string[]> _words;
+
+        public WordSelector(Random random)
+        {
+            _random = random;
+
+            _words = new Dictionary<TopicEnum, string[]>
+            {
+                { TopicEnum.COUNTRIES, new string[] { "Latvia", "Estonia", "Denmark", "China", "Ireland", "Thailand", "Zimbabwe", "France", "Uganda", "Poland", "Sweden" } },
+                { TopicEnum.CITIES, new string[] { "Vilnius", "Kaunas", "Venice", "Kedainiai", "Paris", "Mumbai", "Philadelphia", "Dallas", "Kolkata", "Pasvalys", "Madrid" } },
+                { TopicEnum.SEAS, new string[] { "Java", "Solomon", "White", "Sargasso", "Baltic", "Celebes", "labrador", "Norwegian", "Weddell", "Caribbean", "Greenland" } }
+            };
+
+        }
+
+        public string SelectWord(TopicEnum? topic)
+        {
+            if (topic == null) return "";
+
+            return _words[(TopicEnum)topic][_random.Next(_words[(TopicEnum)topic].Length)].ToUpper();
+        }
+    }
+
+    public class NewWordSelector : WordSelector
+    {
+        public NewWordSelector(Random random, List<string> moreWords) : base(random)
+        {
+            var mas = _words[TopicEnum.COUNTRIES];
+            var newMas = mas.ToList();
+            newMas.AddRange(moreWords);
+            _words[TopicEnum.COUNTRIES] = newMas.ToArray();
+        }
+    }
+
+    public class NewWordSelector_v2_0 : NewWordSelector
+    {
+        public NewWordSelector_v2_0(Random random, List<string> moreWords) : base(random, moreWords)
+        {
+        }
+    }
 }
